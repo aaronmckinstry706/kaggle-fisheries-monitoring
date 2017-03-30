d7562b9389db503e4ed2143cfa8208d089cf824f
Aaron <aaronmckinstry706@gmail.com>
2017-03-30 15:02:20 -0400

    Changed batch_size to 64 (128 was too slow/memory-intensive; also, it was set to 4 in previous commit). Fixed cross entropy loss expression: eliminated extra tensor.sum() calls, and stopped averaging over individual units. Added statements to print execution time for each training epoch. Changed output to say 'Epoch' instead of 'Iteration'. Changed some variable names for clarity.

--- simple_network.py
+++ simple_network.py
@@ -4,6 +4,7 @@
 
 import logging
 import os
+import time
 
 import lasagne
 import lasagne.layers as layers
@@ -21,12 +22,12 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 # Set the preprocessing parameters.
-image_width = 512
+image_width = 128
 
 # Set the learning algorithm parameters.
 learning_rate = theano.shared(numpy.float32(0.01))
 momentum = 0.9
-batch_size = 4
+batch_size = 64
 
 # Set some architectural parameter invariants.
 num_classes = 8
@@ -41,24 +42,28 @@ output_layer = layers.DenseLayer(
     incoming=input_layer, num_units=num_classes,
     nonlinearity=nonlinearities.softmax, name='output_layer')
 
-network_parameters = layers.get_all_params(output_layer)
 outputs = layers.get_output(output_layer)
-unaveraged_losses = tensor.sum(tensor.sum(objectives.categorical_crossentropy(
-    outputs, labels)))
-loss = tensor.sum(unaveraged_losses) / (batch_size*8)
+
+network_parameters = layers.get_all_params(output_layer)
+
+unaveraged_batch_crossentropy_losses = objectives.categorical_crossentropy(
+    outputs, labels)
+batch_crossentropy_loss = (
+    tensor.sum(unaveraged_batch_crossentropy_losses) / batch_size)
 
 logger.info('Compiling the train, test, and validation functions...')
 
 network_updates = updates.nesterov_momentum(
-    loss_or_grads=loss,
+    loss_or_grads=batch_crossentropy_loss,
     params=network_parameters,
     learning_rate=learning_rate,
     momentum=momentum)
 train = theano.function(
     inputs=[inputs, labels],
-    outputs=[outputs, loss],
+    outputs=[outputs, batch_crossentropy_loss],
     updates=network_updates)
-validate = theano.function(inputs=[inputs, labels], outputs=[outputs, loss])
+validate = theano.function(inputs=[inputs, labels],
+                           outputs=[outputs, batch_crossentropy_loss])
 test = theano.function(inputs=[inputs], outputs=[outputs])
 
 logger.info('Initializing data loader...')
@@ -67,9 +72,19 @@ data_loader = loader.Loader(batch_size, image_width,
                             'data/train',
                             'data/test')
 
+logger.info('Pre-loading data...')
+
+data_loader.get_shuffled_train_input_iterator()
+data_loader.get_validation_input_iterator()
+data_loader.get_test_input_iterator()
+
 logger.info('Training model...')
 
-for i in range(0,100):
+num_epochs = 100
+
+for i in range(0, num_epochs):
+    epoch_start_time = time.time()
+    
     train_iterator = data_loader.get_shuffled_train_input_iterator()
     avg_batch_train_loss = 0
     num_iterations = 0
@@ -88,10 +103,13 @@ for i in range(0,100):
         num_iterations += 1
     avg_batch_validate_loss /= num_iterations
     
+    epoch_end_time = time.time()
+    
     print('Iteration ' + str(i) + ': ')
-    print('    Average batch loss over validation set: '
-          + str(avg_batch_validate_loss))
-    print('    Average batch loss over training set: '
-          + str(avg_batch_train_loss))
+    print('    Average batch batch_crossentropy_loss over validation set: '
+          + str(avg_batch_validate_loss) + '.')
+    print('    Average batch batch_crossentropy_loss over training set: '
+          + str(avg_batch_train_loss) + '.')
+    print('    Time: ' + str(epoch_end_time - epoch_start_time) + ' seconds.')
 
 logger.info('Finished.')
