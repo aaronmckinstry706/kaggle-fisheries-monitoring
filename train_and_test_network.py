43b81b0d03f7ade8d7e5ada4f20548a6b6f87991
Aaron <aaronmckinstry706@gmail.com>
2017-03-30 21:48:44 -0400

    Added a real convolutional network. Downsampled images to 256x256 due to memory constraints. Added batch normalization.

--- train_and_test_network.py
+++ train_and_test_network.py
@@ -22,7 +22,7 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 # Set the preprocessing parameters.
-image_width = 128
+image_width = 256
 
 # Set the learning algorithm parameters.
 learning_rate = theano.shared(numpy.float32(0.01))
@@ -32,39 +32,46 @@ batch_size = 64
 # Set architectural parameter invariants.
 num_classes = 8
 
-# Define the symbolic variables and expressions for network computation.
+# Define the symbolic variables and expressions for network computations.
 inputs = tensor.tensor4(name='inputs')
 labels = tensor.matrix(name='labels')
 
 input_layer = layers.InputLayer(shape=(None, 3, image_width, image_width),
-                                input_var=inputs, name='input_layer')
+                                input_var=inputs)
+
+hidden_layer_one_no_batchnorm = layers.Conv2DLayer(incoming=input_layer,
+                                                   num_filters=64,
+                                                   filter_size=(3,3),
+                                                   pad='same')
+hidden_layer_one = layers.batch_norm(hidden_layer_one_no_batchnorm)
+
 output_layer = layers.DenseLayer(
-    incoming=input_layer, num_units=num_classes,
-    nonlinearity=nonlinearities.softmax, name='output_layer')
+    incoming=hidden_layer_one, num_units=num_classes,
+    nonlinearity=nonlinearities.softmax)
 
-outputs = layers.get_output(output_layer)
+train_outputs = layers.get_output(output_layer, deterministic=False)
+test_outputs = layers.get_output(output_layer, deterministic=True)
 
-network_parameters = layers.get_all_params(output_layer)
+network_parameters = layers.get_all_params(output_layer, trainable=True)
 
-unaveraged_batch_crossentropy_losses = objectives.categorical_crossentropy(
-    outputs, labels)
-batch_crossentropy_loss = (
-    tensor.sum(unaveraged_batch_crossentropy_losses) / batch_size)
-
-logger.info('Compiling the train, test, and validation functions...')
+train_loss = tensor.sum(
+    objectives.categorical_crossentropy(train_outputs, labels)) / batch_size
+validation_loss = objectives.categorical_crossentropy(test_outputs, labels)
 
 network_updates = updates.nesterov_momentum(
-    loss_or_grads=batch_crossentropy_loss,
+    loss_or_grads=train_loss,
     params=network_parameters,
     learning_rate=learning_rate,
     momentum=momentum)
+
+logger.info('Compiling the train and validation functions...')
+
 train = theano.function(
     inputs=[inputs, labels],
-    outputs=[outputs, batch_crossentropy_loss],
+    outputs=[train_outputs, train_loss],
     updates=network_updates)
 validate = theano.function(inputs=[inputs, labels],
-                           outputs=[outputs, batch_crossentropy_loss])
-test = theano.function(inputs=[inputs], outputs=[outputs])
+                           outputs=[test_outputs, validation_loss])
 
 logger.info('Initializing data loader...')
 
@@ -95,19 +102,19 @@ for i in range(0, num_epochs):
     avg_batch_train_loss /= num_iterations
     
     validation_iterator = data_loader.get_validation_input_iterator()
-    avg_batch_validate_loss = 0
-    num_iterations = 0
+    avg_validate_loss = 0
+    num_examples = 0
     for images_labels in validation_iterator:
         outputs, validation_loss = validate(images_labels[0], images_labels[1])
-        avg_batch_validate_loss += validation_loss
-        num_iterations += 1
-    avg_batch_validate_loss /= num_iterations
+        avg_validate_loss += numpy.sum(validation_loss)
+        num_examples += images_labels[0].shape[0]
+    avg_validate_loss /= num_examples
     
     epoch_end_time = time.time()
     
     print('Epoch ' + str(i) + ': ')
     print('    Average batch batch_crossentropy_loss over validation set: '
-          + str(avg_batch_validate_loss) + '.')
+          + str(avg_validate_loss) + '.')
     print('    Average batch batch_crossentropy_loss over training set: '
           + str(avg_batch_train_loss) + '.')
     print('    Time: ' + str(epoch_end_time - epoch_start_time) + ' seconds.')
