008884564db9ff5355cb59ccf338b5f1a1aabc90
Aaron <aaronmckinstry706@gmail.com>
2017-04-05 14:11:05 -0400

    Added unit tests for separate/recombine functions from utilities.py in utilities_test.py.

--- utilities_test.py
+++ utilities_test.py
@@ -1,7 +1,7 @@
 import unittest
 import utilities
 
-class UtilitiesTest(unittest.TestCase):
+class UtilitiesTest_PathReaders(unittest.TestCase):
     
     def test_get_labels(self):
         labels = utilities.get_labels('unit_test_resources/data/train')
@@ -18,3 +18,31 @@ class UtilitiesTest(unittest.TestCase):
         self.assertTrue('img_00165.jpg' in relative_paths[0])
         self.assertTrue('img_00325.jpg' in relative_paths[1])
         self.assertTrue('img_00348.jpg' in relative_paths[2])
+
+class UtilitiesTest_FileMovers(unittest.TestCase):
+    
+    def test00_separate_validation_set(self):
+        utilities.separate_validation_set('unit_test_resources/data/train',
+                                          'unit_test_resources/data/validation',
+                                          1.0)
+        self.assertEqual(
+            0,
+            len(utilities.get_relative_paths('unit_test_resources/data/train')))
+        self.assertEqual(
+            184,
+            len(
+                utilities.get_relative_paths(
+                    'unit_test_resources/data/validation')))
+    
+    def test01_recombine_validation_and_training(self):
+        utilities.recombine_validation_and_training(
+            'unit_test_resources/data/validation',
+            'unit_test_resources/data/train')
+        self.assertEqual(
+            184,
+            len(utilities.get_relative_paths('unit_test_resources/data/train')))
+        self.assertEqual(
+            0,
+            len(
+                utilities.get_relative_paths(
+                    'unit_test_resources/data/validation')))
